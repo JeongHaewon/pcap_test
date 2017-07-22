9a54c64cfc7848634ce7394bb43b038c3fbbe4fc
JeongHaewon <onlysun217@gmail.com>
2017-07-23 03:40:52 +0900

    updating

--- main.c
+++ main.c
@@ -1,106 +1,88 @@
 #include <pcap.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stdint.h>
+#include <arpa/inet.h>
+#include <netinet/ip.h>
 
-typedef struct arphdr{
+typedef struct ether_header
+{
+    u_char dmac[6];
+    u_char smac[6];
+    uint16_t type;
+}ethhdr_t;
 
-    u_char dmac [6];
-    u_char smac [6];
-
-}arphdr_t;
-
-typedef struct iphdr{
-
-    u_char sip [4];
-    u_char dip [4];
-
+typedef struct ip_header
+{
+    u_char vsnhdrl[1];
+    uint8_t difsrv;
+    uint16_t tlen;
+    uint16_t identification;
+    uint16_t frgoffset;
+    uint8_t ttlive;
+    uint8_t protocol;
+    uint16_t hdrcsum;
+    uint32_t sip;
+    uint32_t dip;
 }iphdr_t;
 
-typedef struct tcphdr{
-
-    u_char sport [2];
-    u_char dport [2];
-
+typedef struct tcp_header
+{
+    uint16_t sport;
+    uint16_t dport;
+    uint32_t sqnum;
+    uint32_t acknum;
+    u_char hdrl[1];
 }tcphdr_t;
 
-int main(void){
-
-    pcap_t *handle;			/* Session handle */
-    char *dev= "wlan0";			/* The device to sniff on */
-    char errbuf[256];	/* Error string */
-    struct bpf_program fp;		/* The compiled filter */
-    char filter_exp[] = "port 80";	/* The filter expression */
-    bpf_u_int32 mask;		/* Our netmask */
-    bpf_u_int32 net;		/* Our IP */
-    struct pcap_pkthdr *header;	/* The header that pcap gives us */
-    const u_char *pkt_data;		/* The actual packet */
-    int res;
-    int i;
-    arphdr_t *arpheader = NULL;
+int main(int arvc,char* argv[])
+{
+    int snaplen;
+    int promisc;
+    int to_ms;
+    char errbuf[256];
+    ethhdr_t *ethheader = NULL;
     iphdr_t *ipheader = NULL;
     tcphdr_t *tcpheader = NULL;
-    const char *payload;
 
-    /* Define the device */
-    dev = pcap_lookupdev(errbuf);
-    if (dev == NULL) {
-        fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
-        return(2);
+    struct pcap_pkthdr pkthdr;
+    const u_char pkt_data;
+    const u_char *payload;
+
+    pcap_t *handle;
+    handle = pcap_open_live("ens33", snaplen, promisc, to_ms, errbuf);
+    /*    if (pcap_open_live == NULL);
+    {
+        printf("Couldn't open device %s: %s\n", "ens33", errbuf);
+        return 0;
     }
-    /* Open the session in promiscuous mode */
-    handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
-    if (handle == NULL) {
-        fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
-        return(2);
-    }
-    /* Compile and apply the filter */
-    if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
-        fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
-        return(2);
-    }
-    if (pcap_setfilter(handle, &fp) == -1) {
-        fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
-        return(2);
-    }
-    /* Grab a packet */
+*/
+    int number, i;
     while(1){
-       res = pcap_next_ex(handle, &header, &pkt_data);
+        number = pcap_next_ex(handle, &pkthdr, &pkt_data);
+        if(number == 0, 1);
+        {
+            ethheader = (struct ethhdr_t *)(pkt_data);
+            ipheader = (struct iphdr_t *)(pkt_data+14);
+            //  int iplen;
+            //  iplen = ipheader->vsnhdrl
+            tcpheader = (struct tcphdr_t *)(pkt_data+14+20);
 
-       if(res == 0 || pkt_data == NULL)
-            continue;
-       else {
-            printf("Jacked a packet with length of [%d]\n", (*header).len);
-            arpheader = (struct arphr *)(pkt_data);
-            ipheader = (struct iphdr *)(pkt_data+26);
-            tcpheader = (struct tcphdr *)(pkt_data+34);
+            if(ntohs(ethheader->type) == 0x0800);
+            {
+                printf("Packet Lengh is: %d\n", pkthdr.len);
+                printf("Destination MAC: %s\n", ntohl(ethheader->dmac));
+                printf("Source MAC: %s\n", ntohl(ethheader->smac));
+                printf("\n");
+                printf("Source IP: %d\n", ntohl(ipheader->sip));
+                printf("Destination IP: %d\n", ntohl(ipheader->dip));
+                printf("\n");
+                printf("Source port: %d\n", ntohs(tcpheader->sport));
+                printf("Destination port: %d\n", ntohs(tcpheader->dport));
 
-
-            printf("Destination MAC: ");
-            for(i=0; i<6;i++)printf("%02x:", arpheader->dmac[i]);
-            printf("\nSource MAC: ");
-            for(i=0; i<6;i++)printf("%02x:", arpheader->smac[i]);
-
-            printf("\nSource IP: ");
-            for(i=0; i<4;i++)printf("%d.", ipheader->sip[i]);
-            printf("\nDestination IP: ");
-            for(i=0; i<4; i++)printf("%d.", ipheader->dip[i]);
-
-            printf("\nSender TCP Port: ");
-            for(i=0; i<2; i++)printf("%d", tcpheader->sport[i]);
-            printf("\nTarget TCP Port: ");
-            for(i=0; i<2; i++)printf("%d", tcpheader->dport[i]);
-
-
-            printf("\n\nData: ");
-            payload = (u_char *)(pkt_data+54);
-            printf(payload);
-            printf("\n====================================================\n");
-
-
-       }
-
-
+                payload = (u_char *)(pkt_data+14+20+20);
+                printf(payload);
+                printf("\n-------------------------------------------------\n");
+            }
+        }
     }
-return 0;
-
+    return 0;
 }
