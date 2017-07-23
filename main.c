ef8e1b99a80026400b0ed4e81f26a99dc123b8b7
JeongHaewon <onlysun217@gmail.com>
2017-07-23 17:02:14 +0900

    except counting 14 20

--- main.c
+++ main.c
@@ -5,9 +5,9 @@
 
 typedef struct ether_header
 {
-    u_char dmac[6];
-    u_char smac[6];
-    uint16_t type;
+    uint8_t dmac[6];
+    uint8_t smac[6];
+    uint16_t *type;
 }ethhdr_t;
 
 typedef struct ip_header
@@ -35,17 +35,18 @@ typedef struct tcp_header
 
 int main(int arvc,char* argv[])
 {
-    int snaplen;
-    int promisc;
-    int to_ms;
+    int snaplen=2048;
+    int promisc=1;
+    int to_ms=512;
     char errbuf[256];
     ethhdr_t *ethheader = NULL;
-    iphdr_t *ipheader = NULL;
+    iphdr_t *ipheader;
     tcphdr_t *tcpheader = NULL;
 
-    struct pcap_pkthdr pkthdr;
-    const u_char pkt_data;
+    struct pcap_pkthdr *pkthdr;
+    const u_char *pkt_data;
     const u_char *payload;
+
 
     pcap_t *handle;
     handle = pcap_open_live("ens33", snaplen, promisc, to_ms, errbuf);
@@ -56,33 +57,50 @@ int main(int arvc,char* argv[])
     }
 */
     int number, i;
-    while(1){
-        number = pcap_next_ex(handle, &pkthdr, &pkt_data);
-        if(number == 0, 1);
-        {
-            ethheader = (struct ethhdr_t *)(pkt_data);
-            ipheader = (struct iphdr_t *)(pkt_data+14);
-            //  int iplen;
-            //  iplen = ipheader->vsnhdrl
-            tcpheader = (struct tcphdr_t *)(pkt_data+14+20);
+    char sbuf[100], dbuf[100];
 
-            if(ntohs(ethheader->type) == 0x0800);
+    while(1)
+    {
+        number = pcap_next_ex(handle, &pkthdr, &pkt_data);
+        if(number != 1 || pkt_data == NULL)
+        {
+            continue;
+        }
+        ethheader = (struct ethhdr_t *)(pkt_data);
+        ipheader = (struct iphdr_t *)(pkt_data+14);
+        //  int iplen;
+        //  iplen = ipheader->vsnhdrl
+        tcpheader = (struct tcphdr_t *)(pkt_data+14+20);
+
+
+        if(ntohs(ethheader->type) == 0x0800);
+        {
+            inet_ntop(AF_INET, &ipheader->sip, &sbuf, 16);
+            inet_ntop(AF_INET, &ipheader->dip, &dbuf, 16);
+            if(ntohs(tcpheader->sport) == 80 || ntohs(tcpheader->dport))
             {
-                printf("Packet Lengh is: %d\n", pkthdr.len);
-                printf("Destination MAC: %s\n", ntohl(ethheader->dmac));
-                printf("Source MAC: %s\n", ntohl(ethheader->smac));
+                printf("Packet Lengh is: %d\n", pkthdr->len);
+                printf("Destination MAC: ");
+                for(i=0; i<6;i++)printf("%02x:", ethheader->dmac[i]);
+                printf("\nSource MAC: ");
+                for(i=0; i<6;i++)printf("%02x:", ethheader->smac[i]);
                 printf("\n");
-                printf("Source IP: %d\n", ntohl(ipheader->sip));
-                printf("Destination IP: %d\n", ntohl(ipheader->dip));
+                printf("Source IP: %s\n", sbuf);
+                printf("Destination IP: %s\n", dbuf);
                 printf("\n");
                 printf("Source port: %d\n", ntohs(tcpheader->sport));
                 printf("Destination port: %d\n", ntohs(tcpheader->dport));
 
                 payload = (u_char *)(pkt_data+14+20+20);
                 printf(payload);
+
                 printf("\n-------------------------------------------------\n");
             }
         }
+
     }
     return 0;
 }
+
+
+
