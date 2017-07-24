ce99365ccc4a0a7c12bcf1c8cb40fd6c9d6ad8e9
JeongHaewon <onlysun217@gmail.com>
2017-07-24 14:34:36 +0900

    error

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <pcap.h>
+#include <stdio.h>
 #include <stdint.h>
 #include <arpa/inet.h>
 #include <netinet/ip.h>
@@ -12,7 +13,7 @@ typedef struct ether_header
 
 typedef struct ip_header
 {
-    u_char vsnhdrl[1];
+    uint8_t versionheaderl;
     uint8_t difsrv;
     uint16_t tlen;
     uint16_t identification;
@@ -30,8 +31,9 @@ typedef struct tcp_header
     uint16_t dport;
     uint32_t sqnum;
     uint32_t acknum;
-    u_char hdrl[1];
+    uint8_t hdrl;
 }tcphdr_t;
+
 
 int main(int arvc,char* argv[])
 {
@@ -47,38 +49,37 @@ int main(int arvc,char* argv[])
     const u_char *pkt_data;
     const u_char *payload;
 
-
     pcap_t *handle;
     handle = pcap_open_live("ens33", snaplen, promisc, to_ms, errbuf);
-    /*    if (pcap_open_live == NULL);
-    {
-        printf("Couldn't open device %s: %s\n", "ens33", errbuf);
-        return 0;
-    }
-*/
+
     int number, i;
     char sbuf[100], dbuf[100];
+
 
     while(1)
     {
         number = pcap_next_ex(handle, &pkthdr, &pkt_data);
-        if(number != 1 || pkt_data == NULL)
+
+        if(number != 1,0 || pkt_data == NULL)
         {
             continue;
         }
+
+
         ethheader = (struct ethhdr_t *)(pkt_data);
         ipheader = (struct iphdr_t *)(pkt_data+14);
-        //  int iplen;
-        //  iplen = ipheader->vsnhdrl
-        tcpheader = (struct tcphdr_t *)(pkt_data+14+20);
-
+        tcpheader = (struct tcphdr_t *)(pkt_data+14+((ipheader->versionheaderl)&15)*4);
 
         if(ntohs(ethheader->type) == 0x0800);
         {
-            inet_ntop(AF_INET, &ipheader->sip, &sbuf, 16);
-            inet_ntop(AF_INET, &ipheader->dip, &dbuf, 16);
+
             if(ntohs(tcpheader->sport) == 80 || ntohs(tcpheader->dport))
             {
+
+
+                inet_ntop(AF_INET, &ipheader->sip, &sbuf, 16);
+                inet_ntop(AF_INET, &ipheader->dip, &dbuf, 16);
+
                 printf("Packet Lengh is: %d\n", pkthdr->len);
                 printf("Destination MAC: ");
                 for(i=0; i<6;i++)printf("%02x:", ethheader->dmac[i]);
@@ -91,7 +92,7 @@ int main(int arvc,char* argv[])
                 printf("Source port: %d\n", ntohs(tcpheader->sport));
                 printf("Destination port: %d\n", ntohs(tcpheader->dport));
 
-                payload = (u_char *)(pkt_data+14+20+20);
+                payload = (u_char *)(pkt_data+14+((ipheader->versionheaderl)&15)*4+((tcpheader->hdrl)&240)/16*4);
                 printf(payload);
 
                 printf("\n-------------------------------------------------\n");
